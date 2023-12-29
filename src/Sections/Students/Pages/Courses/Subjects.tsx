fd04f34745048eb2fc15ea9f2034867885185e8d
Amal C P <acp070146@gmail.com>
2023-12-29 21:02:51 +0530

    new pages added

---
+++ src/Sections/Students/Pages/Courses/Subjects.tsx
@@ -0,0 +1,9 @@
+
+
+type Props = {}
+
+export const Subjects = (_props: Props) => {
+  return (
+    <div>Subjects</div>
+  )
+}
---
+++ src/Sections/Students/Pages/ProgressTracker/ProgressTracker.tsx
@@ -0,0 +1,9 @@
+
+
+type Props = {}
+
+export const ProgressTracker = (_props: Props) => {
+  return (
+    <div>ProgressTracker</div>
+  )
+}
