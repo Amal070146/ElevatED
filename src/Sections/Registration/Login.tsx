0419da9c7738f6befdc39474ced0bd6999651d32
Amal C P <acp070146@gmail.com>
2024-01-22 23:50:18 +0530

    structure of signup and login completed with some css

--- src/Sections/Registration/Login.tsx
+++ src/Sections/Registration/Login.tsx
@@ -2,50 +2,53 @@ import { ArrowRightsvg } from "../../assets/svg";
 import styles from "./registration.module.css";
 import Logo from '../../Logo.png'
 import google from '../../assets/google.png'
+import image from './assets/image.png'
 
 type Props = {};
 
 export const Login = (_props: Props) => {
   return (
     <div className={styles.Wrapper}>
-      <div>
-        <div>
+      <div className={styles.ContentWrap}>
+        <div className={styles.TopSection}>
           <div>
             <img src={Logo} alt="" />
             <h2>ElevatED</h2>
           </div>
-          <div>
+          <div className={styles.welc}>
             <p>WELCOME BACK 👋🏻</p>
             <h1>Continue to your Account.</h1>
           </div>
-          <div>
+          <button className={styles.googlesign}>
             <img src={google} alt="" />
             <p>Log In with Google</p>
-          </div>
+          </button>
         </div>
-        <div>
+        <div className={styles.separator}>
           <div></div>
           <p>Or use Email</p>
           <div></div>
         </div>
-        <div>
+        <div className={styles.BottomSection}>
           <div>
-            <div>
+            <div className={styles.inputBox}>
               <p>Email</p>
-              <input type="text" placeholder="anu"/>
+              <input type="text" placeholder="anu" />
             </div>
-            <div>
+            <div className={styles.inputBox}>
               <p>Password</p>
-              <input type="text" placeholder="password"/>
+              <input type="text" placeholder="password" />
             </div>
-            <button>Continue <ArrowRightsvg color="#fff"/></button>
+            <button>
+              Continue <ArrowRightsvg color="#fff" />
+            </button>
           </div>
-          <p>
+          <a href="/signup">
             Are you a Newbie?<b> GET STARTED - IT'S FREE</b>
-          </p>
+          </a>
         </div>
       </div>
-      <div></div>
+      <img src={image} alt="" />
     </div>
   );
 };
--- src/Sections/Registration/Signup.tsx
+++ src/Sections/Registration/Signup.tsx
@@ -1,8 +1,47 @@
-
-type Props = {}
+import { ArrowRightsvg } from "../../assets/svg";
+import styles from "./registration.module.css";
+import Logo from "../../Logo.png";
+import image from "./assets/image.png";
+type Props = {};
 
 export const Signup = (_props: Props) => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className={styles.Wrapper}>
+      <div className={styles.ContentWrap}>
+        <div className={styles.TopSection}>
+          <div>
+            <img src={Logo} alt="" />
+            <h2>ElevatED</h2>
+          </div>
+          <div className={styles.welc}>
+            <h1>WELCOME to ElevatED</h1>
+          </div>
+        </div>
+
+        <div className={styles.BottomSection}>
+          <div>
+            <div className={styles.inputBox}>
+              <p>Email</p>
+              <input type="text" placeholder="anu" />
+            </div>
+            <div className={styles.inputBox}>
+              <p>Password</p>
+              <input type="text" placeholder="password" />
+            </div>
+            <button>
+              START YOUR JOURNEY <ArrowRightsvg color="#fff" />
+            </button>
+          </div>
+          <p>
+            By Signing up toeLEVATed, means you agree to our Privacy Policy and
+            Terms of Service
+          </p>
+          <a href="/login">
+            Already a Member? <b> LOG IN</b>
+          </a>
+        </div>
+      </div>
+      <img src={image} alt="" />
+    </div>
+  );
+};
