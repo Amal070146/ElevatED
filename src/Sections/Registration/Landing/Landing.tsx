3ebf8a28b816b02e08506547c944b17e5f425298
Amal C P <acp070146@gmail.com>
2024-03-07 08:53:23 +0530

    landing page updated

--- src/Sections/Registration/Landing/Landing.tsx
+++ src/Sections/Registration/Landing/Landing.tsx
@@ -1,8 +1,11 @@
 import styles from "./Landing.module.css";
 import Logo from "../../../Logo.png";
 import { useNavigate } from "react-router-dom";
-import  functionImgs  from "../assets/landingPgImgs/quizFunctionImg.png";
-
+import functionImgs from "./landingPgImgs/quizFunctionImg.png";
+import women from './landingPgImgs/women.png'
+import quiz from './landingPgImgs/quiz.png'
+import progress from './landingPgImgs/progress.png'
+import subject from "./landingPgImgs/subject.png";
 type Props = {};
 
 export const Landing = (_props: Props) => {
@@ -14,48 +17,58 @@ export const Landing = (_props: Props) => {
     navigate("/login");
   };
 
-const Functionss =[
-  {
-    name:"Quiz",
-    description:"A platform to check your knowledge and grabbing skills",
-    image: <img src={functionImgs} alt="" />
-  },
-  {
-    name:"Explore Subject",
-    description:"Same course by different mentors",
-    image: <img src={functionImgs} alt="" />
-  }
-]
+  const Functionss = [
+    {
+      name: "Quiz",
+      description: "A platform to check your knowledge and grabbing skills",
+      image: functionImgs,
+    },
+    {
+      name: "Explore Subject",
+      description: "Same course by different mentors",
+      image: quiz,
+    },
+    {
+      name: "Your Progress Tracker",
+      description: "A platform to check your knowledge and grabbing skills",
+      image: progress,
+    },
+    {
+      name: "Explore Subject",
+      description: "Same course by different mentors",
+      image: subject,
+    },
+  ];
 
   return (
     <div className={styles.Wrapper}>
       <div className={styles.TopNavbar}>
         <img src={Logo} alt="" />
         <div className={styles.Registration}>
-        <button onClick={handleLogin}>Login</button>
-        <button onClick={handleSignUP}>SignUp</button>
+          <button onClick={handleLogin}>Login</button>
+          <button onClick={handleSignUP}>SignUp</button>
         </div>
       </div>
 
       <div className={styles.bodyWrapper}>
         <h1>ElevatED an Educational platform</h1>
-          <div className={styles.functionWrapper}>
-         <h3>Functions</h3> 
-         <div className={styles.functionWrapWrapper}>
-         {Functionss.map(({image,name,description}) =>{
-         return(
-         <div className={styles.eachFunctionWrapper}>
-            <h4>{name}</h4>
-            <h5>{description}</h5>
-         </div>
-         );
-         
-         })
-        }
+        <div className={styles.functionWrapper}>
+          <div>
+            <h3>Functions</h3>
+            <div className={styles.functionWrapWrapper}>
+              {Functionss.map(({ image, name, description }) => {
+                return (
+                  <div className={styles.eachFunctionWrapper}>
+                    <img src={image} alt="" />
+                    <h4>{name}</h4>
+                    <h5>{description}</h5>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <img src={women} alt="" />
         </div>
-
-        </div>
-        
       </div>
     </div>
   );
