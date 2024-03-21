adc24b5579e759643f82b22afc776f591f76fbaa
Amal C P <acp070146@gmail.com>
2024-03-21 22:44:02 +0530

    notification page updated

--- src/Pages/Notifications/Notifications.tsx
+++ src/Pages/Notifications/Notifications.tsx
@@ -1,8 +1,52 @@
-
-type Props = {}
+import styles from "./Notifications.module.css";
+import { Bell, CourseNew, QuizReminder } from "./svg";
+type Props = {};
 
 export const Notifications = (_props: Props) => {
   return (
-    <div>Notifications</div>
-  )
-}
+    <div className={styles.Wrapper}>
+      <div className={styles.Header}>
+        <h1>Notifications</h1>
+        <p>Stay informed with the latest updates.</p>
+      </div>
+      <div className={styles.Content}>
+        <div className={styles.Individual}>
+          <div>
+            <Bell />
+            <div>
+              <h3>Class Schedule Update</h3>
+              <p>
+                An educational session has been added to your class schedule
+              </p>
+            </div>
+          </div>
+          <p>2 minutes ago</p>
+        </div>{" "}
+        <div className={styles.Individual}>
+          <div>
+            <CourseNew />
+            <div>
+              <h3>New Course Available</h3>
+              <p>
+                An educational session has been added to your class schedule
+              </p>
+            </div>
+          </div>
+          <p>2 minutes ago</p>
+        </div>{" "}
+        <div className={styles.Individual}>
+          <div>
+            <QuizReminder />
+            <div>
+              <h3>Science Quiz Reminder</h3>
+              <p>
+                An educational session has been added to your class schedule
+              </p>
+            </div>
+          </div>
+          <p>2 minutes ago</p>
+        </div>
+      </div>
+    </div>
+  );
+};
