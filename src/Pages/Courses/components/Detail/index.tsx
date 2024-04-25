b5dc4b36189679c6efcd3db3bf843be6c9f53ff2
Jenin <jenin8282@gmail.com>
2024-04-25 11:05:13 +0530

    feat: pdf view

--- src/Pages/Courses/components/Detail/index.tsx
+++ src/Pages/Courses/components/Detail/index.tsx
@@ -1,13 +1,16 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import styles from "./index.module.css";
 import { VideoSvg } from "./svg";
 import { useEffect, useState } from "react";
 import { supabase } from "../../../../utils/supabase";
 import toast from "react-hot-toast";
+import Modal from "../../../../Components/modal";
 
 export const DetailCourse = () => {
   const { id } = useParams();
   const [course, setCourse] = useState<CourseDisplayType>();
+  const [moduleIndex, setModuleIndex] = useState(0);
+  const [section, setSection] = useState("video");
 
   useEffect(() => {
     featchData();
@@ -49,8 +52,8 @@ export const DetailCourse = () => {
             {course &&
               course.modules &&
               course.modules.length > 0 &&
-              course.modules.map((module) => (
-                <button key={module.id}>
+              course.modules.map((module, index) => (
+                <button key={module.id} onClick={() => setModuleIndex(index)}>
                   <h3>{module.name}</h3>
                   <div>
                     <p>{module.yt_link}</p>
@@ -61,38 +64,60 @@ export const DetailCourse = () => {
           </div>
           <div className={styles.RightPopUpModuleContainer}>
             <div className={styles.Header}>
-              <h2>Module 1</h2>
-              <button>
+              <h2>Module {course?.modules[moduleIndex].name}</h2>
+              {/* <button>
                 <p>Edit</p>
-              </button>
+              </button> */}
             </div>
             <div className={styles.Filter}>
-              <button>All</button>
               <button>Video</button>
-              <button>PDF</button>
-              <button>Syllabus</button>
+              <button onClick={() => setSection("pdf")}>PDF</button>
+              <button>MCQ</button>
+              <button>Long QA</button>
             </div>
             <div className={styles.VideosPDF}>
-              <button>
+              <Link to={course?.modules[moduleIndex].yt_link!}>
                 <VideoSvg />
                 <p>Video 1</p>
-              </button>
-              <button>
-                {" "}
-                <VideoSvg />
-                <p>Video 2</p>
-              </button>
-              <button>
-                <VideoSvg />
-                <p>Video 3</p>
-              </button>
-              <button>
-                {" "}
-                <VideoSvg />
-                <p>Video 4</p>
-              </button>
+              </Link>
             </div>
           </div>
+          <Modal
+            isOpen={section === "pdf"}
+            onClose={() => {
+              setSection("video");
+            }}
+            title={"PDF"}
+            type={"success"}
+          >
+            <div>
+              <h3>Text</h3>
+              <p>{course?.modules[moduleIndex]?.pdf?.text}</p>
+              <h3>Images</h3>
+              {course?.modules[moduleIndex]?.pdf?.image_urls.map((image) => (
+                <img src={image} alt="" />
+              ))}
+            </div>
+          </Modal>
+          <Modal
+            isOpen={section === "mcq"}
+            onClose={() => {
+              setSection("video");
+            }}
+            title={"MCQ"}
+            type={"success"}
+          >
+            <div>
+              <h3>Text</h3>
+              <p>{course?.modules[moduleIndex]?.pdf?.text}</p>
+              <h3>Images</h3>
+              {course?.modules[moduleIndex]?.pdf?.image_urls.map((image) => (
+                <img src={image} alt="" />
+              ))}
+            </div>
+          </Modal>
+
+
 
           {/* <div className={styles.RightPopUpMentorDetailsContainer}>
             <h3>Mentor</h3>
